bf319f0a3c4866e382f55f1bce55bfe4f3a568d0
glasyara <139944286+glasyara@users.noreply.github.com>
2024-10-29 02:24:04 +0000

    teste vercel

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,23 +1,30 @@
 // database.ts
 
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config(); // Certifique-se de carregar as variáveis de ambiente
 
-const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
-    dialect: 'mysql', 
-});
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+  }
+);
 
 // Testando a conexão com o banco de dados
-sequelize.authenticate()
-    .then(() => {
-        console.log('Conexão com o banco de dados estabelecida com sucesso.');
-    })
-    .catch(err => {
-        console.error('Não foi possível conectar ao banco de dados:', err);
-    });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexão com o banco de dados estabelecida com sucesso.");
+  })
+  .catch((err) => {
+    console.error("Não foi possível conectar ao banco de dados:", err);
+  });
 
 export default sequelize; // Exportando a instância do Sequelize
